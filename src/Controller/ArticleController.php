0747d48dc4113315528a6a85ad17884ccc1bdbde
Damir Perkosan <dperkosan@gmail.com>
2020-02-16 19:46:21 +0100

    routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,14 +9,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ArticleController extends AbstractController {
 
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage(){
-        return new Response('OMG! Its alive');
+        // return new Response('OMG! Its alive');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug){
         // return new Response(sprintf(

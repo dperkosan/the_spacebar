b9975db570f5c35bb3df37c2ee43bbfecbae50b1
Damir Perkosan <dperkosan@gmail.com>
2020-02-16 21:12:24 +0100

    api

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController {
@@ -34,7 +35,18 @@ class ArticleController extends AbstractController {
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug){
+        // TODO - actually heart/unheart article
+
+        // return $this->json(['hearts' => rand(5,100)]);
+        return new JsonResponse(['hearts' => rand(5,100)]);
     }
 }

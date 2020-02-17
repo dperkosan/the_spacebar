b68e6855938a2682b3da67817c5335bcd16a3a18
Damir Perkosan <dperkosan@gmail.com>
2020-02-17 11:31:56 +0100

    services and config

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
+use Michelf\MarkdownInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController {
@@ -21,7 +23,7 @@ class ArticleController extends AbstractController {
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug){
+    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache){
         // return new Response(sprintf(
         //     'Some text of article: %s', $slug
         // ));
@@ -34,8 +36,39 @@ class ArticleController extends AbstractController {
 
         dump($slug, $this);
 
+        // get bundle configuration
+        // php bin/console config:dump KnpMarkdownBundle
+        dump($markdown);
+
+        $articleContent = <<<EOF
+        Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+        lorem proident [beef ribs](https.www.beef.com) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+        labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+        **turkey** shank eu pork belly meatball non cupim.
+
+        Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+        laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+        capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+        picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+        occaecat lorem meatball prosciutto quis strip steak.
+
+        Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+        mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+        strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+        cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+        fugiat.
+        EOF;
+
+        $item = $cache->getItem('markdown_'.md5($articleContent));
+        if(!$item->isHit()){
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
+        $articleContent = $item->get();
+
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'articleContent' => $articleContent,
             'slug' => $slug,
             'comments' => $comments
         ]);
